1152114a599a5ddf0376f2694cb0a2bf10a290e2
Roy M J <roymj88@gmail.com>
2015-04-16 16:10:13 +0530

    Quiz 1 answer

---
+++ quiz-simple-aggregation-example.js
@@ -0,0 +1,30 @@
+/*
+Question : 
+
+Write the aggregation query that will find the number of products by category of a collection 
+that has the form:
+{
+	"_id" : ObjectId("50b1aa983b3d0043b51b2c52"),
+	"name" : "Nexus 7",
+	"category" : "Tablets",
+	"manufacturer" : "Google",
+	"price" : 199
+}
+Have the resulting key be called "num_products," as in the video lesson. 
+Hint, you just need to change which key you are aggregating on relative to the 
+examples shown in the lesson.
+
+*/
+
+/*********************Answer***************************/
+
+
+db.products.aggregate([
+{
+    $group: {
+        "_id" : "$category",
+        "num_products" : { $sum : 1}
+    
+    }
+}
+])
