9fada0ddc972840c3f73430cc1bb56bde3f0060f
Roy M J <roymj88@gmail.com>
2015-04-16 16:23:03 +0530

    Quiz - The Aggregation Pipeline

---
+++ quiz-the-aggregation-pipeline.js
@@ -0,0 +1,18 @@
+/*QUIZ: THE AGGREGATION PIPELINE
+
+Which of the following are stages in the aggregation pipeline. Check all that apply.*/
+
+// Correct Ones 
+
+1. Match
+2. Group
+3. Skip
+4. Limit
+5. Sort
+6. Project
+7. Unwind
+
+
+// Not in aggregation pipeline
+
+1. Transpose
